9a60a455556a44db65181dbbb914544023133ff8
Melanie Murillo <mmurillow@est.ups.edu.ec>
2025-06-09 16:47:00 -0500

    variables

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-//funcion para mostrar un cuadro de dialogo  emergente
+//Funcion para mostrar un cuadro de dialogo emergente
 function mostrarMensaje(){
-    alert("Hola mundo");
+    alert("Hola mundo :)");
 }
+
+//Variables
+function imprimirVariables(){
+    let nombre= "Rodnny";
+    let edad= "20"
+//Forma tradicional de conectar cadenas
+    console.log("nombre:", nombre)
+    console.log("edad:", edad)
+//Forma moderna de conectar cadenas
+    console.log(`nombre: ${nombre},edad: ${edad}`);
+    console.error("Error: bla bla bla");
+    console.warn("Advertencia: x x x");
+
+}
+
